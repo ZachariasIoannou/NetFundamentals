f418e85c1ba8b050b5a7966d03879e798d0274ab
Zach Ioannou <zioannou77@gmail.com>
2017-04-24 19:50:55 +0300

    Anonymous methods as delegates

--- EventsDelegatesLambdas/Program.cs
+++ EventsDelegatesLambdas/Program.cs
@@ -12,23 +12,20 @@ namespace EventsDelegatesLambdas
         static void Main(string[] args)
         {
             var worker = new Worker();
-            worker.WorkPerformed += Worker_WorkPerformed;
-            worker.WorkComplete += Worker_WorkComplete1;
+
+            worker.WorkPerformed += delegate(object sender, WorkPerformedEventArgs e)
+            {
+                Console.WriteLine(e.Hours + " " + e.WorkType);
+            };
+
+            worker.WorkComplete += delegate(object sender, EventArgs e)
+            {
+                Console.WriteLine("Work is Complete");
+            };
+
             worker.DoWork(5, WorkType.GoToMeetings);
+
             Console.Read();
         }
-
-        private static void Worker_WorkPerformed(object sender, WorkPerformedEventArgs e)
-        {
-            Console.WriteLine(e.Hours + " " + e.WorkType);
-        }
-
-        private static void Worker_WorkComplete1(object sender, EventArgs e)
-        {
-            Console.WriteLine("Work is Complete");
-        }
-
-
-
     }
 }
