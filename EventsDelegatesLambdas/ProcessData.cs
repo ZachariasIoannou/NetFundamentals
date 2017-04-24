e8a9f08daf1b9ec5fef80500ad7831a1dec62ef7
Zach Ioannou <zioannou77@gmail.com>
2017-04-24 20:30:44 +0300

    using lamdas with custom delegates

---
+++ EventsDelegatesLambdas/ProcessData.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EventsDelegatesLambdas
+{
+    public class ProcessData
+    {
+        public void Process(int x, int y, BizRulesDelegate del)
+        {
+            var result = del(x, y);
+            Console.WriteLine(result);
+
+        }
+    }
+}
--- EventsDelegatesLambdas/Program.cs
+++ EventsDelegatesLambdas/Program.cs
@@ -1,16 +1,26 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace EventsDelegatesLambdas
 {
+    public delegate int BizRulesDelegate(int x, int y);
     class Program
     {
       
         static void Main(string[] args)
         {
+            BizRulesDelegate addDel = (x, y) => x + y;
+            BizRulesDelegate MultiplyDel = (x, y) => x * y;
+            
+            var data = new ProcessData();
+
+            data.Process(2,3,addDel );
+            data.Process(2, 3, MultiplyDel);
+
             var worker = new Worker();
 
             worker.WorkPerformed += delegate(object sender, WorkPerformedEventArgs e)
